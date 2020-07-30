6af184533cc9f645cfaff9130fad1b532b4d51eb
Emmanuel <emmanuel.arnedo@outlook.com.ar>
2020-07-30 17:16:47 -0300

    Juego Maymen inicio

---
+++ jugamos.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void Maymen();
+
+void Maymen(){
+	
+}
--- tp grupal.cpp
+++ tp grupal.cpp
@@ -1,9 +1,5 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "jugamos.h"
 
-//Soy Emmanuel, es una prueba esto
-//holaaaaa
-///Soy un comentario
 main(){
 	int num, juego;
 	printf("\t\t\t\t\t\tBIENVENIDOS\n\n");
@@ -24,7 +20,7 @@ main(){
 			printf("\n\t\t\t\t"); system("pause");
 			system("cls");
 			switch (juego){
-				case 1: printf("\t\t\t\t\tBienvenido a Maymen!");
+				case 1: Maymen();
 				break;
 				case 2: printf("\t\t\t\t\tBienvenido a Adivinum!");
 				break;
